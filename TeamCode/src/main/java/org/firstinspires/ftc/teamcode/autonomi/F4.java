361b062fb7abdbcbb56232f3dda2b2ea941a0ce5
dvirbot <110500588+dvirbot@users.noreply.github.com>
2024-02-26 18:33:27 -0300

    Starts making the Autonomi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomi/F4.java
@@ -0,0 +1,14 @@
+package org.firstinspires.ftc.teamcode.autonomi;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+public class F4 extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        Trajectory leftProp = drive.trajectoryBuilder(new Pose2d())
+    }
+}
